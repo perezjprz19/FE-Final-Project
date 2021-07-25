b0fc2e63b6a81e9d4883ec83c96dad2cc1f0cf3c
Jessica Shellenhamer <perezjprz19@gmail.com>
2021-07-25 12:54:04 -0400

    + fixed positioning to footer. Moved header modal mark-up to top of html.

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,24 @@
-
 //SETS HEADER HEIGHT ON PAGE LOAD
-$(document).ready(function(){
-	$('.header').height($(window).height());
+$(document).ready(function() {
+  $('.header').height($(window).height());
 
 });
 
 //ANIMATES THE HAMBURGER MENU
-$('#hamburger').click(function(){
+$('#hamburger').click(function() {
   $(this).toggleClass("open");
   $('.navbar').toggleClass("close");
 });
 
 //ANIMATES "LET'S TALK" TEXT IN ABOUT SECTION ON FIRST CLICK OF ABOUT LINK IN NAV
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('.about-link').click(function(){
-    $('.contact').css("animation","bounce 4s");
+  $('.about-link').click(function() {
+    $('.contact').css("animation", "bounce 4s");
   });
 });
 
 //TOGGLES SHOWS OR HIDES THE CONTACT FORM IN THE ABOUT SECTION.
-$('.contact').click(function(){
+$('.contact').click(function() {
   $('.contact-form').toggleClass("form-hidden");
 });
