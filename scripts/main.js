ef5ff49e883731db7c1dc3099d0572a817edbcc6
Jessica Shellenhamer <perezjprz19@gmail.com>
2021-07-24 20:42:02 -0400

    added about section

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,15 @@ $('#hamburger').click(function(){
   $(this).toggleClass("open");
   $('.navbar').toggleClass("close");
 });
+
+//ANIMATES "LET'S TALK" TEXT IN ABOUT SECTION ON FIRST CLICK OF ABOUT LINK IN NAV
+$(document).ready(function(){
+
+  $('.about-link').click(function(){
+    $('#contact').css("animation","bounce 4s");
+  });
+});
+
+$('.contact').click(function(){
+  $('#contactForm').toggleClass("form-hidden");
+});
